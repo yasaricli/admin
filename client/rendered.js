f67df29ec3de29fe0ecd56d58ae4f71c9bc1c93f
Yasar icli <hello@yasaricli.com>
2015-04-19 02:28:02 +0300

    update and insert auto focus input.

---
+++ client/rendered.js
@@ -0,0 +1,7 @@
+Template.adminCollectionInsert.onRendered(function() {
+  this.find('input').focus();
+});
+
+Template.adminCollectionUpdate.onRendered(function() {
+  this.find('input').focus();
+});
