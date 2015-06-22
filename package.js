fb3de6dd216018b21b6cd17edd69b936dae0ff2d
Yasar icli <hello@yasaricli.com>
2015-06-22 10:02:55 +0300

    version updated 0.4.0_1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'yasaricli:admin',
-    version: '0.4.0',
+    version: '0.4.0_1',
     summary: 'Meteorjs is the basic admin interface.',
     git: 'https://github.com/yasaricli/admin.git',
     documentation: 'README.md'
